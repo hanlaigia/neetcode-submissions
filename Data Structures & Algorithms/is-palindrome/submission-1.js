51baf1c4bf9716e25004295a112b6eebb238dd8d
HanLai <nhahan0908@gmail.com>
2026-08-22 00:46:06 +0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        s = s.toLowerCase();
+        let i=0;
+        let j=s.length-1;
+        while (i<j){
+            if (!/[a-z0-9]/.test(s[i])){
+                i++;
+                continue;
+            }
+            if (!/[a-z0-9]/.test(s[j])){
+                j--;
+                continue;
+            }
+            if (s[i]!==s[j]){
+                return false
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
