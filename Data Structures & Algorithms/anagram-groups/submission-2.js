0131f30f7e348c0170be8448de184aa2be9393cd
HanLai <nhahan0908@gmail.com>
2026-08-21 21:42:57 +0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.js
@@ -0,0 +1,57 @@
+// class Solution {
+//     /**
+//      * @param {string[]} strs
+//      * @return {string[][]}
+//      */
+//     groupAnagrams(strs) {
+//         const res={}
+//         for (let s of strs){
+//             const sortedS= s.split('').sort().join('');
+//             if (!res[sortedS]){
+//                 res[sortedS]=[];
+//             }
+//             res[sortedS].push(s);
+//         }
+//         return Object.values(res);
+//     }
+// }
+
+// class Solution {
+//     /**
+//      * @param {string[]} strs
+//      * @return {string[][]}
+//      */
+//     groupAnagrams(strs) {
+//         const res={}
+//         for (let s of strs){
+//             const count=new Array(26).fill(0);
+//             for (let c of s){
+//                 count[c.charCodeAt(0)-'a'.charCodeAt(0)]+=1;
+//             }
+//             const key = count.join(',');
+//             if (!res[key]){
+//                 res[key]=[];
+//             }
+//             res[key].push(s);
+//         }
+//         return Object.values(res);
+//     }
+// }
+
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const res={};
+        for (let s of strs){
+            const sorted=s.split('').sort().join('');
+            if(!res[sorted]){
+                res[sorted]=[];
+            }
+            res[sorted].push(s);
+        }
+        return Object.values(res);
+    }
+}
