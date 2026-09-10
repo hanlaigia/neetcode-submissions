c3075a1f0c45c7ee444d4afe8da6d4d41ad9e11f
HanLai <nhahan0908@gmail.com>
2026-09-10 22:22:59 +0700

    Add: duplicate-integer - submission-20

---
+++ Data Structures & Algorithms/duplicate-integer/submission-20.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const map = new Map();
+        for (let i=0; i<nums.length;i++){
+            map.set(nums[i], (map.get(nums[i])||0)+1);
+            if(map.get(nums[i])===2){
+                return true;
+            }
+        }
+        return false;
+    }
+}
